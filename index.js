1183f18f0a038cd25adc1385211b0f59ca69c429
iovoid <io4@users.noreply.github.com>
2017-01-26 18:37:07 -0300

    Make timeout a variable
    Timeout defaults to 3000ms

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 const child_process = require("child_process");
-function execute(script) {
+function execute(script, timeout) {
     var executer = child_process.fork(__dirname+"/lib/sandbox.js");
     return new Promise((resolve,reject) => {
         var timekill = setTimeout(()=>{
             executer.kill("SIGHUP");
             reject("Timeout");
-        },3000);
+        }, timeout || 3000);
         executer.on('message',(m)=>{
             resolve(m);
             clearTimeout(timekill); // prevent issues.
